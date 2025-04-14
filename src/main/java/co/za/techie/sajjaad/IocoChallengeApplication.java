8b8ebd61308eb8eaa73fd789bcd146bf1db51d48
Sajjaad <sajjaadishmail@gmail.com>
2025-04-14 12:34:44 +0200

    initial commit

---
+++ src/main/java/co/za/techie/sajjaad/IocoChallengeApplication.java
@@ -0,0 +1,13 @@
+package co.za.techie.sajjaad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocoChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocoChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/za/techie/sajjaad/IocoChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.techie.sajjaad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocoChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
